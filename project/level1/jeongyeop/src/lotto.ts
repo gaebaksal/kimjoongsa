649a925bd4a6349bdd747c0a3a8b76559c1df70b
nayak <wjdduq914@gmail.com>
2017-12-02 18:22:07 +0900

    Update. 날것의 로또코드

--- project/level1/jeongyeop/src/lotto.ts
+++ project/level1/jeongyeop/src/lotto.ts
@@ -1,48 +1,99 @@
 import * as readline from 'readline';
 
 const r = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-let matchNumbers : number[] = [];
+// 1~45 배열 반환
+function getBaseNumbers(): number[] {
+  let baseNumbers: number[] = [];
 
-while(matchNumbers.length < 7) {
-    const tempNumber : number = Math.floor(Math.random() * (46 - 1) + 1);
-    // TODO 중복 제거
-    matchNumbers.push(tempNumber);
+  for (let i = 1; i <= 45; i++) {
+    baseNumbers.push(i);
+  }
+  return baseNumbers;
 }
 
-// console.log(matchNumbers);
+// 랜덤 숫자 배열 생성
+function generateRandomNumbers(count: number): number[] {
+  let randomNumbers: number[] = [];
+  let baseNumbers = getBaseNumbers();
 
-console.log(`
-지금부터 로또게임을 시작하지.
-0: 수동, 1: 자동
-선택하시게.`);
+  for (let i = 0; i < count; i++) {
+    const index: number = Math.floor(Math.random() * 45 - i);
+    randomNumbers.push(baseNumbers.splice(index, 1)[0]);
+  }
+  return randomNumbers;
+}
+
+let lottoNumbers: number[] = generateRandomNumbers(7);
+const bonusNumber = lottoNumbers[6];
+
+console.log(
+  `지금부터 로또게임을 시작하지.
+  0: 수동, 1: 자동
+  선택하시게.`
+);
 
 r.setPrompt('> ');
 r.prompt();
 
-r.on('line', (line : string) => {
-    if (line === 'exit') {
-        r.close();
-    }
-    if (line === '0') {
-        console.log('7자리 숫자를 입력하세요.');
-        line.split(' ').forEach((item) => {
-            parseInt(item, 10);
-        });
-    }
-    if (line === '1') {
-        console.log('자동 선택');
-    }
-    if (line.length >= 13) {
-        console.log(line);
+let userNumbers: number[] = [];
+r.on('line', (input: string) => {
+  if (input === 'exit') {
+    r.close();
+  }
+  // 수동
+  if (input === '0') {
+    console.log('6자리 숫자를 입력하세요.');
+    r.prompt();
+  }
+  // 자동
+  if (input === '1') {
+    console.log('자동 선택');
+    userNumbers = generateRandomNumbers(6);
+  }
+  // 수동 입력
+  if (input.length >= 11) {
+    input.split(' ').forEach((item) => {
+      userNumbers.push(parseInt(item, 10));
+    });
+  }
+
+  // 당첨 확인
+  if (userNumbers.length > 0) {
+    let matchCount = 0;
+    userNumbers.forEach((userNumber) => {
+      (lottoNumbers.indexOf(userNumber) !== -1) ? matchCount++ : '';
+    });
+
+    switch (matchCount) {
+      case 3:
+        console.log('5등'); break;
+      case 4:
+        console.log('4등'); break;
+      case 5:
+        console.log('3등'); break;
+      case 6:
+        if (userNumbers.indexOf(bonusNumber) !== -1) {
+          console.log('2등');
+        } else {
+          console.log('1등');
+        }
+        break;
+      default:
+        console.log('꽝');
     }
 
-    r.prompt();
+    lottoNumbers.pop();
+    console.log(`로또 번호: ${lottoNumbers} + bonus: ${bonusNumber}`);
+    console.log(`내 번호: ${userNumbers}`);
+
+    r.close();
+  }
 });
 
 r.on('close', () => {
-    process.exit();
+  process.exit();
 });
